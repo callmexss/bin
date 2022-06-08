edbfa816016348aba1b1f92c82050bc0da65c21d
callmexss <callmexss@126.com>
2022-06-08 23:17:01 +0800

    Add fy.py for quick translation and .gitignore file.

---
+++ fy.py
@@ -0,0 +1,26 @@
+import click
+import clipboard
+import translators as ts
+
+
+def translate(query, to_language):
+    for provider in [ts.youdao, ts.google, ts.bing, ts.caiyun]:
+        if ret := provider(query, to_language=to_language):
+            return ret
+
+
+@click.command()
+@click.argument("QUERY", default="")
+def fy(query):
+    if not query:
+        query = clipboard.paste()
+
+    to_lang = "en" if any([not x.isascii() for x in query]) else "zh"
+    result = translate(query, to_lang)
+    print(result)
+    clipboard.copy(result)
+    input()
+
+
+if __name__ == "__main__":
+    fy()
